c12b8c12fa99aea2ec3788a4457a48d26b3f52a4
Norbert Langner <norbitor@gmail.com>
2019-06-11 13:22:49 +0200

    Add Cognito auth to react app

--- src/App.js
+++ src/App.js
@@ -1,6 +1,11 @@
+import Amplify from 'aws-amplify'
+import aws_exports from './aws-exports'
+import { withAuthenticator } from 'aws-amplify-react';
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+
+Amplify.configure(aws_exports);
 
 function App() {
   return (
@@ -23,4 +28,4 @@ function App() {
   );
 }
 
-export default App;
+export default withAuthenticator(App, { includeGreetings: true });
