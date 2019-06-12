9663e0e603b114916e2f8ec5555c14a75edd9eb2
Mateusz <mateusz.laczkowski@gmail.com>
2019-06-12 16:34:22 +0200

    add storage, newPet, subs

--- src/App.js
+++ src/App.js
@@ -2,10 +2,12 @@ import Amplify from 'aws-amplify'
 import aws_exports from './aws-exports'
 import { withAuthenticator } from 'aws-amplify-react';
 import { Connect } from 'aws-amplify-react';
-import { graphqlOperation } from 'aws-amplify';
-import { Grid, Header, Input, List, Segment } from 'semantic-ui-react';
+import { API, graphqlOperation, Storage } from 'aws-amplify';
+import { Form, Grid, Header, Input, List, Segment } from 'semantic-ui-react';
+import {v4 as uuid} from 'uuid';
 
 import React from 'react';
+import { Component } from 'react';
 import './App.css';
 
 Amplify.configure(aws_exports);
@@ -25,6 +27,58 @@ function makeComparator(key, order='asc') {
   };
 }
 
+class S3ImageUpload extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { uploading: false }
+  }
+  onChange = async (e) => {
+    const file = e.target.files[0];
+    const fileName = uuid();
+    this.setState({uploading: true});
+    const result = await Storage.put(
+      fileName, 
+      file, 
+      {
+        customPrefix: { public: 'uploads/' },
+        metadata: { petid: this.props.petId }
+      }
+    );
+    console.log('Uploaded file: ', result);
+    this.setState({uploading: false});
+  }
+  render() {
+    return (
+      <div>
+        <Form.Button
+          onClick={() => document.getElementById('add-image-file-input').click()}
+          disabled={this.state.uploading}
+          icon='file image outline'
+          content={ this.state.uploading ? 'Uploading...' : 'Add Image' }
+        />
+        <input
+          id='add-image-file-input'
+          type="file"
+          accept='image/*'
+          onChange={this.onChange}
+          style={{ display: 'none' }}
+        />
+      </div>
+    );
+  }
+}
+
+class PetDetails extends Component {
+  render() {
+    return (
+      <Segment>
+        <Header as='h3'>{this.props.pet.name}</Header>
+        <S3ImageUpload petId={this.props.pet.id}/>        
+        <p>TODO: Show photos for this pet</p>
+      </Segment>
+    )
+  }
+}
 class PetsList extends React.Component {
   petItems() {
     return this.props.pets.sort(makeComparator('name')).map(pet =>
@@ -56,9 +110,18 @@ const ListPets = `query ListPets {
 }`;
 
 class PetsListLoader extends React.Component {
+  onNewPet = (prevQuery, newData) => {
+    let updatedQuery = Object.assign({}, prevQuery);
+    updatedQuery.ListPets.items = prevQuery.ListPets.items.concat([newData.onCreatePet]);
+    return updatedQuery;
+  }
   render() {
     return (
-      <Connect query={graphqlOperation(ListPets)}>
+      <Connect query={graphqlOperation(ListPets)}
+                subscription={graphqlOperation(SubscribeToNewPets)} 
+                onSubscriptionMsg={this.onNewPet}
+                 >
+        
         {({ data, loading, errors }) => {
           if (loading) { return <div>Loading...</div> }
           if (!data.listPets) return;
@@ -69,15 +132,87 @@ class PetsListLoader extends React.Component {
   }
 }
 
-function App() {
-  return (
-    <Grid padded>
-      <Grid.Column>
-        Ala ma kota
-        <PetsListLoader />
-      </Grid.Column>
-    </Grid>
-  );
+
+class NewPet extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      petName: '',
+      petSpecies: ''
+     };
+   }
+
+  handleChange = (event) => {
+    let change = {};
+    change[event.target.name] = event.target.value;
+    this.setState(change);
+  }
+
+//, race:"$race", bornYear:"$born"
+  handleSubmit = async (event) => {
+    event.preventDefault();
+    const NewPet = `mutation createPet($name: String!, $species: String!) {
+      createPet(input: {name:"$name", species:"$species"}) {
+        id
+        name
+      }
+}`;
+    console.info(`this.state.petName ${this.state.petName}`);
+    const result = await API.graphql(graphqlOperation(NewPet, { name: this.state.petName, species: this.state.petSpecies}));
+    console.info(`Created pet with id ${result.data.createPet.id}`);
+  }
+
+  render() {
+    return (
+      <Segment>
+        <Header as='h3'>Add a new pet</Header>
+         <Input
+          type='text'
+          placeholder='New Pet Name'
+          icon='plus'
+          iconPosition='left'
+          
+          name='petName'
+          value={this.state.petName}
+          onChange={this.handleChange}
+         />
+          <Input
+          type='text'
+          placeholder='New Pet Species'
+          icon='plus'
+          iconPosition='left'
+          action={{ content: 'Create', onClick: this.handleSubmit }}
+          name='petSpecies'
+          value={this.state.petSpecies}
+          onChange={this.handleChange}
+         />
+        </Segment>
+      )
+   }
+}
+
+const SubscribeToNewPets = `
+  subscription OnCreatePet {
+    onCreatePet {
+      id
+      name
+    }
+  }
+`;
+
+
+
+class App extends Component {
+  render() {
+    return (
+      <Grid padded>
+        <Grid.Column>
+          <NewPet />
+          <PetsListLoader />
+        </Grid.Column>
+      </Grid>
+    );
+  }
 }
 
 export default withAuthenticator(App, { includeGreetings: true });
